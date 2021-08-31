7510a25bfed7014358679618519359318771044b
Lucia Cerchie <lucia@stepzen.com>
2021-08-31 13:22:58 -0700

    fixes import of provider
    fixes import of provider-- used to import from apollo/react-hooks

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloProvider } from "@apollo/react-hooks"
+import { ApolloProvider } from "@apollo/client"
 import ApolloClient from "apollo-boost"
 
 const {
