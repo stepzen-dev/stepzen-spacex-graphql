b7507f660d8e6c99ce502e65f15901b19bffac5b
Rebecca Key <rek990@gmail.com>
2021-10-09 18:27:09 -0400

    Imported and implemented Card component into App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { gql, useQuery } from "@apollo/client";
 import StepZenLogo from "./light-blue.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Rockets from "./rocket-information";
+import Card from "./Card";
 
 const GET_QUERY = gql`
   query MyQuery {
@@ -57,16 +58,18 @@ function Home() {
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
-          <p style={{ marginTop: "40px" }}>Capsule information pulled from StepZen Endpoint:</p>
-          <span>id: {data.capsule.id}</span>
-          <span>landings: {data.capsule.landings}</span>
-          <span>reuse-count: {data.capsule.reuse_count}</span>
-          <Link to="/rockets">Rockets Information</Link>
-        </header>
-      </div>
+      <Card>
+        <div className="App">
+          <header className="App-header">
+            <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
+            <p style={{ marginTop: "40px" }}>Capsule information pulled from StepZen Endpoint:</p>
+            <span>id: {data.capsule.id}</span>
+            <span>landings: {data.capsule.landings}</span>
+            <span>reuse-count: {data.capsule.reuse_count}</span>
+            <Link to="/rockets">Rockets Information</Link>
+          </header>
+        </div>
+      </Card>
     </>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 const Card = (props) => {
   const classes = "card " + props.className;
-  return <div className={classes}></div>;
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Card;
