2d69cd779d328158d6d3164ff41a2a0fa8dbe26b
Rebecca Key <rek990@gmail.com>
2021-10-09 18:00:05 -0400

    Removed <ul> and created <span> element for data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import { gql, useQuery } from '@apollo/client';
-import StepZenLogo from './light-blue.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Rockets from './rocket-information';
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import StepZenLogo from "./light-blue.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Rockets from "./rocket-information";
 
 const GET_QUERY = gql`
-  query MyQuery   {
+  query MyQuery {
     capsule(id: "C105") {
       id
       landings
@@ -20,8 +20,6 @@ const GET_QUERY = gql`
 `;
 
 function App() {
-
-  
   return (
     <>
       <Router>
@@ -52,30 +50,24 @@ function App() {
 
 function Home() {
   const { loading, error, data } = useQuery(GET_QUERY);
-  console.log('DATA', data)
+  console.log("DATA", data);
 
   if (loading) return <p>Loading ...</p>;
 
-  if (error) return (
-    <pre>{JSON.stringify(error, null, 2)}</pre>
-  );
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
   return (
     <>
       <div className="App">
         <header className="App-header">
           <img src={StepZenLogo} alt="StepZen Logo" width="200px" />
           <p style={{ marginTop: "40px" }}>Capsule information pulled from StepZen Endpoint:</p>
-            <ul>
-              <li>id: {data.capsule.id}</li>
-              <li>landings: {data.capsule.landings}</li>
-              <li>reuse-count: {data.capsule.reuse_count}</li>
-            </ul>
-          <Link to='/rockets'>Rockets Information</Link>
+          <span>id: {data.capsule.id}</span>
+          <span>landings: {data.capsule.landings}</span>
+          <span>reuse-count: {data.capsule.reuse_count}</span>
+          <Link to="/rockets">Rockets Information</Link>
         </header>
       </div>
     </>
   );
-
 }
-;
 export default App;
